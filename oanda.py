63b94681430b710c2b53078e234e16dc664fd337
Eddie Shin <eshinhw@gmail.com>
2023-09-01 22:02:25 -0400

    updated oanda api

--- oanda.py
+++ oanda.py
@@ -1,17 +1,16 @@
 import pandas as pd
 import datetime as dt
 from oandapyV20 import API
-from pprint import pprint
-from typing import List, Dict, Tuple
 import oandapyV20.endpoints.pricing as pricing
 import oandapyV20.endpoints.accounts as accounts
 import oandapyV20.endpoints.instruments as instruments
 import oandapyV20.endpoints.orders as orders
 import oandapyV20.endpoints.trades as trades
 
+from typing import List, Dict, Tuple
+
 
 class Oanda:
-
     def __init__(self, api_key, accountID) -> None:
         self.client = API(api_key)
         self.acctID = accountID
@@ -27,7 +26,7 @@ class Oanda:
         data = {"Date": [], "Open": [], "High": [], "Low": [], "Close": []}
 
         for candle in resp["candles"]:
-            date = candle['time'].replace('T', ' ')[:candle['time'].index('.')]
+            date = candle["time"].replace("T", " ")[: candle["time"].index(".")]
             data["Date"].append(dt.datetime.strptime(date, "%Y-%m-%d %H:%M:%S"))
             data["Open"].append(float(candle["mid"]["o"]))
             data["High"].append(float(candle["mid"]["h"]))
@@ -39,27 +38,23 @@ class Oanda:
         return df
 
     def get_current_low(self, symbol, count, interval):
-        r = instruments.InstrumentsCandles(instrument=symbol,
-                                           params={"count": count,
-                                                    "granularity": interval,
-                                                    "dailyAlignment": 17})
+        r = instruments.InstrumentsCandles(
+            instrument=symbol, params={"count": count, "granularity": interval, "dailyAlignment": 17}
+        )
         resp = self.client.request(r)
-        most_recent_low = float(resp['candles'][-1]['mid']['l'])
+        most_recent_low = float(resp["candles"][-1]["mid"]["l"])
         return most_recent_low
 
     def get_current_high(self, symbol, count, interval):
-        r = instruments.InstrumentsCandles(instrument=symbol,
-                                           params={"count": count,
-                                                    "granularity": interval,
-                                                    "dailyAlignment": 17})
+        r = instruments.InstrumentsCandles(
+            instrument=symbol, params={"count": count, "granularity": interval, "dailyAlignment": 17}
+        )
         resp = self.client.request(r)
-        most_recent_high = float(resp['candles'][-1]['mid']['h'])
+        most_recent_high = float(resp["candles"][-1]["mid"]["h"])
         return most_recent_high
 
-
     def get_current_ask_bid_price(self, symbol: str) -> Tuple[float]:
-        r = pricing.PricingInfo(accountID=self.acctID,
-                                params={"instruments": symbol})
+        r = pricing.PricingInfo(accountID=self.acctID, params={"instruments": symbol})
         resp = self.client.request(r)
         ask_price = float(resp["prices"][0]["closeoutAsk"])
         bid_price = float(resp["prices"][0]["closeoutBid"])
@@ -67,7 +62,7 @@ class Oanda:
 
     def calculate_MA(self, symbol: str, period: int, interval: str):
         df = self.get_ohlc(symbol, period + 3, interval)
-        ma = df['Close'].rolling(period).mean().iloc[-1]
+        ma = df["Close"].rolling(period).mean().iloc[-1]
         return ma
 
     def calculate_ATR(self, symbol: str, period: int, interval: str):
@@ -83,25 +78,20 @@ class Oanda:
 
     def calculate_prev_min_low(self, symbol: str, period: int, interval: str):
         df = self.get_ohlc(symbol, period, interval)
-        return min(df['Low'])
+        return min(df["Low"])
 
     def calculate_prev_max_high(self, symbol: str, period: int, interval: str):
         df = self.get_ohlc(symbol, period, interval)
-        return max(df['High'])
+        return max(df["High"])
 
-    def calculate_unit_size(self,
-                            symbol: str,
-                            entry: float,
-                            stop: float,
-                            risk: float):
-
+    def calculate_unit_size(self, symbol: str, entry: float, stop: float, risk: float):
         # https://www.youtube.com/watch?v=bNEpAOOulwk&ab_channel=KarenFoo
 
         account_balance = self.get_balance()
         decimal = 4
         multiple = 10000
 
-        usdcad = self.get_current_ask_bid_price('USD_CAD')[0]
+        usdcad = self.get_current_ask_bid_price("USD_CAD")[0]
         us_dolloar_per_trade = (account_balance * risk) / usdcad
         entry = round(entry, decimal)
         stop = round(stop, decimal)
@@ -119,23 +109,21 @@ class Oanda:
                 if order["type"] == "LIMIT" and trade["instrument"] == order["instrument"]:
                     self.cancel_single_order(order["id"])
 
-
     def get_order_details(self, order_ID: str) -> Dict:
         r = orders.OrderDetails(accountID=self.acctID, orderID=order_ID)
         resp = self.client.request(r)
-        return (resp['order'])
-
+        return resp["order"]
 
     def find_order_id(self, symbol: str, direction: str) -> str:
         order_list = self.get_order_list()
 
         for order in order_list:
-            if direction == 'LONG':
-                if order['type'] == 'LIMIT' and order['instrument'] == symbol and not ('-' in order['units']):
-                    return order['id']
-            if direction == 'SHORT':
-                if order['type'] == 'LIMIT' and order['instrument'] == symbol and ('-' in order['units']):
-                    return order['id']
+            if direction == "LONG":
+                if order["type"] == "LIMIT" and order["instrument"] == symbol and not ("-" in order["units"]):
+                    return order["id"]
+            if direction == "SHORT":
+                if order["type"] == "LIMIT" and order["instrument"] == symbol and ("-" in order["units"]):
+                    return order["id"]
 
     def check_open_order(self, symbol: str):
         order_list = self.get_order_list()
@@ -158,11 +146,9 @@ class Oanda:
                 r = trades.TradeClose(accountID=self.acctID, tradeID=trade["id"])
                 self.client.request(r)
 
-
     def cancel_single_order(self, order_ID: str) -> None:
         r = orders.OrderCancel(accountID=self.acctID, orderID=order_ID)
         self.client.request(r)
-
 
     def close_all_trades(self) -> None:
         trades_list = self.get_trade_list()
@@ -180,71 +166,65 @@ class Oanda:
                 self.client.request(r)
             order_list = self.get_order_list()
 
-
     def get_order_list(self) -> List[Dict]:
-        """ Retrieve a list of open orders"""
+        """Retrieve a list of open orders"""
         r = orders.OrderList(self.acctID)
         resp = self.client.request(r)
-        return resp['orders']
+        return resp["orders"]
 
     def get_trade_list(self) -> List[Dict]:
-        """ Retrieve a list of open trades"""
+        """Retrieve a list of open trades"""
         r = trades.TradesList(self.acctID)
         resp = self.client.request(r)
-        return resp['trades']
+        return resp["trades"]
 
     def symbols_in_orders(self):
         orders = self.get_order_list()
         symbols = []
-        #pprint(orders)
+        # pprint(orders)
         if orders:
             for order in orders:
-                #pprint(order)
-                if order['type'] != 'STOP_LOSS' and order['instrument'] not in symbols:
-                    #print(order['instrument'])
-                    symbols.append(order['instrument'])
+                # pprint(order)
+                if order["type"] != "STOP_LOSS" and order["instrument"] not in symbols:
+                    # print(order['instrument'])
+                    symbols.append(order["instrument"])
         return symbols
 
     def symbols_in_trades(self):
         trades = self.get_trade_list()
         symbols = []
-        #pprint(trades)
+        # pprint(trades)
         if trades:
             for trade in trades:
-                #pprint(trade)
-                if trade['instrument'] not in symbols:
-                    symbols.append(trade['instrument'])
+                # pprint(trade)
+                if trade["instrument"] not in symbols:
+                    symbols.append(trade["instrument"])
         return symbols
 
     def create_limit_order(self, symbol, entry, stop, risk):
         (units, entry, stop, distance) = self.calculate_unit_size(symbol, entry, stop, risk)
-        # print(entry-stop)
-        # print("entry:", entry)
-        # print('stop:', stop)
-        # print(entry < stop)
-        # print(entry > stop)
         # Sell Limit
         if entry < stop:
-            #print('sell limit')
+            # print('sell limit')
             order_body = {
-            "order": {
-                "price": str(entry),
-                "stopLossOnFill": {"timeInForce": "GTC", "price": str(stop)},
-                "trailingStopLossOnFill": {"timeInForce": "GTC", "distance": str(distance)},
-                "timeInForce": "GTC",
-                "instrument": symbol,
-                "units": "-" + str(units),
-                "type": "LIMIT",
-                "positionFill": "DEFAULT",
+                "order": {
+                    "price": str(entry),
+                    "stopLossOnFill": {"timeInForce": "GTC", "price": str(stop)},
+                    "trailingStopLossOnFill": {"timeInForce": "GTC", "distance": str(distance)},
+                    "timeInForce": "GTC",
+                    "instrument": symbol,
+                    "units": "-" + str(units),
+                    "type": "LIMIT",
+                    "positionFill": "DEFAULT",
                 }
             }
-            #print(order_body)
+            # print(order_body)
             r = orders.OrderCreate(self.acctID, data=order_body)
             self.client.request(r)
 
         # Buy Limit
         else:
-            #print('buy limit')
+            # print('buy limit')
             order_body = {
                 "order": {
                     "price": str(entry),
@@ -257,7 +237,7 @@ class Oanda:
                     "positionFill": "DEFAULT",
                 }
             }
-            #print(order_body)
+            # print(order_body)
             r = orders.OrderCreate(self.acctID, data=order_body)
             self.client.request(r)
 
@@ -265,9 +245,9 @@ class Oanda:
         trades_list = self.get_trade_list()
 
         for trade in trades_list:
-            if symbol == trade['instrument']:
-                sl_order_id = trade['stopLossOrder']['id']
-                trade_id = trade['id']
+            if symbol == trade["instrument"]:
+                sl_order_id = trade["stopLossOrder"]["id"]
+                trade_id = trade["id"]
                 # cancel existing stop loss
                 self.cancel_single_order(sl_order_id)
                 # create new stop loss
@@ -276,26 +256,25 @@ class Oanda:
                         "type": "STOP_LOSS",
                         "tradeID": trade_id,
                         "price": str(new_stop_loss),
-                        "timeInForce": "GTC"
+                        "timeInForce": "GTC",
                     }
                 }
                 r = orders.OrderCreate(self.acctID, data=order_body)
                 self.client.request(r)
-
 
     def create_stop_order(self, symbol, entry, stop, risk):
         (units, entry, stop, distance) = self.calculate_unit_size(symbol, entry, stop, risk)
         # Sell Stop
         if entry < stop:
             order_body = {
-            "order": {
-                "price": str(entry),
-                "stopLossOnFill": {"timeInForce": "GTC", "price": str(stop)},
-                "timeInForce": "GTC",
-                "instrument": symbol,
-                "units": "-" + str(units),
-                "type": "STOP",
-                "positionFill": "DEFAULT",
+                "order": {
+                    "price": str(entry),
+                    "stopLossOnFill": {"timeInForce": "GTC", "price": str(stop)},
+                    "timeInForce": "GTC",
+                    "instrument": symbol,
+                    "units": "-" + str(units),
+                    "type": "STOP",
+                    "positionFill": "DEFAULT",
                 }
             }
             r = orders.OrderCreate(self.acctID, data=order_body)
@@ -315,10 +294,3 @@ class Oanda:
             }
             r = orders.OrderCreate(self.acctID, data=order_body)
             self.client.request(r)
-
-if __name__ == "__main__":
-    od = Oanda()
-
-
-
-
