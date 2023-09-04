06fe38400b9dd4d742a3f88849cebf7de7f614bf
Eddie Shin <eshinhw@gmail.com>
2023-09-04 18:56:04 -0400

    merge

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from trade import entry
 
-INSTRUMENTS = ["EUR_USD", "GBP_USD"]
+INSTRUMENTS = ["EUR_USD", "GBP_USD", "AUD_USD", "NZD_USD"]
 
 
 def main():
     for ins in INSTRUMENTS:
         entry(ins)
+
+
+if __name__ == "__main__":
+    main()
