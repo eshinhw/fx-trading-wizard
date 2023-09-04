809c3c74d98999f41f8c81d8a21b4b80ff6d66b4
Eddie Shin <eshinhw@gmail.com>
2023-09-04 18:46:06 -0400

    implemented trading setup and check_crossover

--- trade.py
+++ trade.py
@@ -1,14 +1,18 @@
 import os
-from oanda import Oanda
-from config import API_KEY, ACCOUNT_NUM
 
+import numpy as np
+
+from config import ACCOUNT_NUM, ACCOUNT_CURRENCY, API_KEY
+from config import SMA, LMA, INTERVAL, SL_PERCENT, RISK_PER_TRADE
+from config import MAJOR, EUR_CROSS, JPY_CROSS, GBP_CROSS, OTHER_CROSS
+from oanda import Oanda
 
 # Login
 
 if os.name == "nt":
     oanda = Oanda(API_OANDA, CROSSOVER_ACCT)
 if os.name == "posix":
-    oanda = Oanda(API_KEY, ACCOUNT_NUM)
+    oanda = Oanda(API_KEY, ACCOUNT_NUM, ACCOUNT_CURRENCY)
 
 OPEN_ORDERS = oanda.get_order_list()
 OPEN_TRADES = oanda.get_trade_list()
@@ -16,52 +20,35 @@ OPEN_TRADES = oanda.get_trade_list()
 SYMBOLS_ORDERS = oanda.symbols_in_orders()
 SYMBOLS_TRADES = oanda.symbols_in_trades()
 
-# DAILY SETUP
-SMA = 50
-LMA = 200
-INTERVAL = "D"
-MA_LAGGING_PERIOD = -3
-SL_PERCENT = 0.025
-RISK_PER_TRADE = 0.02
 
-
-def bullish_crossover_test(symbol):
-    ohlc = oanda.get_ohlc(symbol, LMA * 2, INTERVAL)
+def check_crossover(symbol):
+    prices = oanda.get_prices(symbol, 365, INTERVAL)
     sma = f"{SMA}MA"
     lma = f"{LMA}MA"
-    ohlc[sma] = ohlc["Close"].rolling(SMA).mean()
-    ohlc[lma] = ohlc["Close"].rolling(LMA).mean()
+    prices[sma] = prices["Close"].rolling(int(SMA)).mean()
+    prices[lma] = prices["Close"].rolling(int(LMA)).mean()
+    prices["50MA_lagged"] = prices[sma].shift(5)
+    prices.dropna(inplace=True)
 
-    prev_sma = ohlc[sma].iloc[MA_LAGGING_PERIOD]
-    prev_lma = ohlc[lma].iloc[MA_LAGGING_PERIOD]
+    def find_crossover(fast_sma, lagged_fast_sma, slow_sma):
+        if fast_sma > slow_sma and lagged_fast_sma < slow_sma:
+            return "bullish"
+        if fast_sma < slow_sma and lagged_fast_sma > slow_sma:
+            return "bearish"
+        return "neutral"
 
-    curr_sma = ohlc[sma].iloc[-1]
-    curr_lma = ohlc[lma].iloc[-1]
+    prices["crossover"] = np.vectorize(find_crossover)(prices[sma], prices["50MA_lagged"], prices[lma])
 
-    return prev_sma < prev_lma and curr_sma > curr_lma
+    print(prices)
 
-
-def bearish_crossover_test(symbol):
-    ohlc = oanda.get_ohlc(symbol, LMA * 2, INTERVAL)
-    sma = f"{SMA}MA"
-    lma = f"{LMA}MA"
-    ohlc[sma] = ohlc["Close"].rolling(SMA).mean()
-    ohlc[lma] = ohlc["Close"].rolling(LMA).mean()
-
-    prev_sma = ohlc[sma].iloc[MA_LAGGING_PERIOD]
-    prev_lma = ohlc[lma].iloc[MA_LAGGING_PERIOD]
-
-    curr_sma = ohlc[sma].iloc[-1]
-    curr_lma = ohlc[lma].iloc[-1]
-
-    return prev_sma > prev_lma and curr_sma < curr_lma
+    return prices["crossover"].iloc[-1]
 
 
 def entry(symbol):
     print(f"Testing MA Crossover for {symbol}")
     try:
         # bullish cross over --> long
-        if bullish_crossover_test(symbol):
+        if check_crossover(symbol) == "bullish":
             # Determine entry and stop price
             entry = oanda.get_current_ask_bid_price(symbol)[0]
             stop = entry - (entry * SL_PERCENT)
@@ -75,7 +62,7 @@ def entry(symbol):
                 oanda.create_limit_order(symbol, entry, stop, RISK_PER_TRADE)
 
         # bearish cross over --> short
-        if bearish_crossover_test(symbol):
+        elif check_crossover(symbol) == "bearish":
             # Determine entry and stop price
             entry = oanda.get_current_ask_bid_price(symbol)[1]
             stop = entry + (entry * SL_PERCENT)
@@ -92,9 +79,17 @@ def entry(symbol):
         print(e)
 
 
-# if __name__ == "__main__":
-#     # open_trades('EURUSD')
-#     # print("Run Successfully --> " + time.ctime())
+if __name__ == "__main__":
+    instruments = MAJOR + EUR_CROSS + JPY_CROSS + GBP_CROSS + OTHER_CROSS
+    print(instruments)
+    # check_crossover("EUR_USD")
+    # print(INSTRUMENTS)
+    # print(oanda.calculate_unit_size("EUR_USD", 1.06319, 1.05010, 0.02))
+    # print(oanda.calculate_unit_size("AUD_CAD", 0.8634, 0.85118, 0.02))
+    # print(oanda.calculate_unit_size("EUR_NZD", 1.81088, 1.77822, 0.02))
+    # print(oanda.calculate_unit_size("GBP_AUD", 1.94612, 1.90252, 0.02))
+    # print(oanda.calculate_unit_size("CAD_CHF", 0.66069, 0.67701, 0.02))
+    # print(oanda.calculate_unit_size("CAD_JPY", 106.53, 105.02, 0.02))
+    # print(oanda.cal_position_size("EURUSD", RISK_PER_TRADE, 1.06))
 
-#     o = Oanda(API_KEY, ACCOUNT_NUM)
-#     # print(o.calculate_MA("EUR_USD", 20, "D"))
+    entry(instruments[0])
